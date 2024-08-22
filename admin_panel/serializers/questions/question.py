745dcefd5069213e68987c4ac7a9191891e3a702
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:01:16 +0530

    handled question response

--- admin_panel/serializers/questions/question.py
+++ admin_panel/serializers/questions/question.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from admin_panel.models import Question
+from admin_panel.serializers.university.subject import SubjectSerializer
+from admin_panel.serializers.university.topic import TopicSerializer
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+
+class QuestionResponseSerializer(serializers.ModelSerializer):
+    subject = SubjectSerializer()
+    topic = TopicSerializer()
+
     class Meta:
         model = Question
         fields = "__all__"
--- admin_panel/viewsets/questions/question.py
+++ admin_panel/viewsets/questions/question.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 
 from admin_panel.models import Question
-from admin_panel.serializers.questions import QuestionSerializer
+from admin_panel.serializers.questions import (
+    QuestionSerializer,
+    QuestionResponseSerializer,
+)
 
 
 class QuestionViewset(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return QuestionResponseSerializer
+        return QuestionSerializer
