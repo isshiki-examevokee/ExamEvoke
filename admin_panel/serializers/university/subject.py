9f98777dddd2ea7149dc106340b6a93a226b4e6f
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:01:59 +0530

    handled subject response

--- admin_panel/serializers/university/subject.py
+++ admin_panel/serializers/university/subject.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from admin_panel.models import Subject
+from admin_panel.serializers.university.batch import BatchSerializer
 
 
 class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = "__all__"
+
+
+class SubjectResponseSerializer(serializers.ModelSerializer):
+    batch = BatchSerializer()
+
     class Meta:
         model = Subject
         fields = "__all__"
--- admin_panel/viewsets/university/subject.py
+++ admin_panel/viewsets/university/subject.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from admin_panel.models import Subject
-from admin_panel.serializers.university.subject import SubjectSerializer
+from admin_panel.serializers.university.subject import SubjectSerializer, SubjectResponseSerializer
 
 
 class SubjectViewset(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return SubjectResponseSerializer
+        return SubjectSerializer
