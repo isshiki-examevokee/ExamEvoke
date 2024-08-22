44be6196eb69f9c092bdb6d15882f87eafa27903
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:05:34 +0530

    added pagination

--- examevoke/settings.py
+++ examevoke/settings.py
@@ -82,6 +82,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination', # noqa
+    'PAGE_SIZE': 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework.authentication.TokenAuthentication',
     ],
