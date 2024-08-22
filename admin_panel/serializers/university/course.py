e4ab9b37ccf4330daf16cd5d272a9b19ee2408f7
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:01:27 +0530

    handled course response

--- admin_panel/serializers/university/course.py
+++ admin_panel/serializers/university/course.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from admin_panel.models import Course
+from admin_panel.serializers.university.organization import OrganizationSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class CourseResponseSerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer()
+
     class Meta:
         model = Course
         fields = "__all__"
--- admin_panel/viewsets/university/course.py
+++ admin_panel/viewsets/university/course.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 
 from admin_panel.models import Course
-from admin_panel.serializers.university.course import CourseSerializer
+from admin_panel.serializers.university.course import (
+    CourseSerializer,
+    CourseResponseSerializer,
+)
 
 
 class CourseViewset(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CourseResponseSerializer
+        return CourseSerializer
