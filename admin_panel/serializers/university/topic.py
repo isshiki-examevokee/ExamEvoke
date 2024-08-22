4cdd7d5e392613a98fec4db5e9f4b2ede16a72c8
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:01:43 +0530

    handled topic response

--- admin_panel/serializers/university/topic.py
+++ admin_panel/serializers/university/topic.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from admin_panel.models import Topic
+from admin_panel.serializers.university.batch import BatchSerializer
 
 
 class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = "__all__"
+
+
+class TopicResponseSerializer(serializers.ModelSerializer):
+    batch = BatchSerializer()
+
     class Meta:
         model = Topic
         fields = "__all__"
--- admin_panel/viewsets/university/topic.py
+++ admin_panel/viewsets/university/topic.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from admin_panel.models import Topic
-from admin_panel.serializers.university.topic import TopicSerializer
+from admin_panel.serializers.university.topic import TopicSerializer, TopicResponseSerializer
 
 
 class TopicViewset(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return TopicResponseSerializer
+        return TopicSerializer
