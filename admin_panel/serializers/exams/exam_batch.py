7c39169c475e42d5e8e5d81a445b0f5ad057abc9
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:00:42 +0530

    handled exam_batch response

--- admin_panel/serializers/exams/exam_batch.py
+++ admin_panel/serializers/exams/exam_batch.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from admin_panel.models import ExamBatch
+from admin_panel.serializers.exams.exam import ExamSerializer
 
 
 class ExamBatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamBatch
+        fields = "__all__"
+
+
+class ExamBatchResponseSerializer(serializers.ModelSerializer):
+    exam = ExamSerializer()
+
     class Meta:
         model = ExamBatch
         fields = "__all__"
--- admin_panel/viewsets/exams/exam_batch.py
+++ admin_panel/viewsets/exams/exam_batch.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 
 from admin_panel.models import ExamBatch
-from admin_panel.serializers.exams.exam_batch import ExamBatchSerializer 
+from admin_panel.serializers.exams.exam_batch import (
+    ExamBatchSerializer,
+    ExamBatchResponseSerializer
+) 
 
 
 class ExamBatchViewset(viewsets.ModelViewSet):
     queryset = ExamBatch.objects.all()
     serializer_class = ExamBatchSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return ExamBatchResponseSerializer
+        return ExamBatchSerializer
