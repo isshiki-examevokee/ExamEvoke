b511484a6e4ad78db208d038aa61223ab456d673
Jeevan <justjeevan07@gmail.com>
2024-08-22 13:02:13 +0530

    handled batch response

--- admin_panel/serializers/university/batch.py
+++ admin_panel/serializers/university/batch.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from admin_panel.models import Batch
+from admin_panel.serializers.university.course import CourseSerializer
 
 
 class BatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Batch
+        fields = "__all__"
+
+
+class BatchResponseSerializer(serializers.ModelSerializer):
+    course = CourseSerializer()
+
     class Meta:
         model = Batch
         fields = "__all__"
--- admin_panel/viewsets/university/batch.py
+++ admin_panel/viewsets/university/batch.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from admin_panel.models import Batch
-from admin_panel.serializers.university.batch import BatchSerializer
+from admin_panel.serializers.university.batch import BatchResponseSerializer, BatchSerializer
 
 
 class BatchViewset(viewsets.ModelViewSet):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return BatchResponseSerializer
+        return BatchSerializer
